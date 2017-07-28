6e94ea0f91cea566ab1b2777e79d329abd542d3c
stack user <stack@localhost>
2017-07-28 21:15:46 +0900

    user/registry cleanup.

--- freeradius/do.sh
+++ freeradius/do.sh
@@ -6,10 +6,13 @@ oc create -f ./configs/svc_externalip.yaml
 oc create -f ./configs/bc_radius.yaml
 oc create -f ./configs/dc_radius.yaml
 
+user=${USER}
+registry=$(sudo oc get route/docker-registry --template='{{.spec.host}}')
+
 sudo docker pull ubuntu:16.04
-sudo docker login -p $(oc whoami -t) -u test docker-registry-default.app.example.com
-sudo docker tag docker.io/ubuntu:16.04 docker-registry-default.app.example.com/radius/ubuntu:16.04
-sudo docker push docker-registry-default.app.example.com/radius/ubuntu:16.04
+sudo docker login -p $(oc whoami -t) -u ${USER} ${registry}
+sudo docker tag docker.io/ubuntu:16.04 ${registry}/radius/ubuntu:16.04
+sudo docker push ${registry}/radius/ubuntu:16.04
 oc start-build radius --follow --from-dir .
 
 sleep 5
